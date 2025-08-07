59c94fbee58c38359e35c79172b39e12050117d1
Dmitry Komkov <dmtkomkov@gmail.com>
2025-08-07 16:57:13 +0200

    Designed initial labels and added them to the step page

--- src/app/home/step-page/step-groups/step-groups.component.ts
+++ src/app/home/step-page/step-groups/step-groups.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { IStepGroup } from '../../../models/step';
+import { IStep, IStepGroup } from '../../../models/step';
 import { StepService } from '../../../services/step.service';
 import { RouterLink } from '@angular/router';
 import { Observable, startWith, switchMap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { ILabelData, LabelComponent } from '../../../shared/label/label.component';
+import { ColorUtils } from '../../../utils/color-utils';
 
 @Component({
   selector: 'sg-step-groups',
-  imports: [RouterLink, AsyncPipe],
+  imports: [RouterLink, AsyncPipe, LabelComponent],
   templateUrl: './step-groups.component.html',
   styleUrl: './step-groups.component.scss',
 })
@@ -24,5 +26,37 @@ export class StepGroupsComponent {
       startWith(undefined),
       switchMap(() => this.stepService.getStepGroupsAsync()),
     );
+  }
+
+  getLabelData(step: IStep): ILabelData {
+    const labelItems = [];
+    labelItems.push({
+      text: step.user.userName,
+      color: '#eeeeee',
+      background: '#777888'
+    });
+    labelItems.push({
+      text: step.category.name,
+      color: '#000000',
+      background: this.getTransparentColor(step.category.area.color),
+    });
+    if (step.goal) {
+      labelItems.push({
+        text: step.goal.name,
+        color: '#000000',
+        background: this.getTransparentColor(step.goal.project?.color),
+      });
+    }
+    labelItems.push({
+      text: step.duration.toString(),
+      color: '#eeeeee',
+      background: '#777888'
+    });
+
+    return { data: labelItems };
+  }
+
+  getTransparentColor(color: string | undefined): string {
+    return ColorUtils.hexToRgba(color || ColorUtils.defaultColor, 0.5);
   }
 }
---
+++ src/app/shared/label/label.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+export interface ILabelItem {
+  text: string;
+  color: string;
+  background: string;
+}
+
+export interface ILabelData {
+  data: ILabelItem[];
+}
+
+@Component({
+  selector: 'sg-label',
+  imports: [],
+  templateUrl: './label.component.html',
+  styleUrl: './label.component.scss'
+})
+export class LabelComponent {
+  @Input() labelData: ILabelData = { data: [] };
+}
