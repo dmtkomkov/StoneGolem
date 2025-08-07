c7b697cdf85cb9282f603afafc01ae71e218d75c
Dmitry Komkov <dmtkomkov@gmail.com>
2025-08-07 17:44:17 +0200

    Added labels for goals and categories

--- src/app/home/category-page/catgory-groups/category-groups.component.ts
+++ src/app/home/category-page/catgory-groups/category-groups.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { Observable, startWith, switchMap } from 'rxjs';
 import { CategoryService } from '../../../services/category.service';
-import { ICategoryGroup } from '../../../models/category';
+import { ICategoryFlat, ICategoryGroup } from '../../../models/category';
 import { RouterLink } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { ColorUtils } from '../../../utils/color-utils';
+import { ILabelData, LabelComponent } from '../../../shared/label/label.component';
 
 @Component({
   selector: 'sg-category-groups',
-  imports: [RouterLink, AsyncPipe],
+  imports: [RouterLink, AsyncPipe, LabelComponent],
   templateUrl: './category-groups.component.html',
   styleUrl: './category-groups.component.scss'
 })
@@ -33,5 +34,16 @@ export class CategoryGroupsComponent {
 
   getTransparentColor(group: ICategoryGroup): string {
     return ColorUtils.hexToRgba(group.area.color, ColorUtils.defaultOpacity);
+  }
+
+  getLabelData(category: ICategoryFlat): ILabelData {
+    const labelItems = [];
+    labelItems.push({
+      text: category.name,
+      color: '#000000',
+      background: '#eeeeee',
+    });
+
+    return { data: labelItems };
   }
 }
--- src/app/home/goal-page/goal-groups/goal-groups.component.ts
+++ src/app/home/goal-page/goal-groups/goal-groups.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { Observable, startWith, switchMap } from 'rxjs';
-import { IGoalGroup } from '../../../models/goal';
+import { IGoalFlat, IGoalGroup } from '../../../models/goal';
 import { GoalService } from '../../../services/goal.service';
 import { ColorUtils } from '../../../utils/color-utils';
+import { ILabelData, LabelComponent } from '../../../shared/label/label.component';
 
 @Component({
   selector: 'sg-goal-groups',
-  imports: [RouterLink, AsyncPipe],
+  imports: [RouterLink, AsyncPipe, LabelComponent],
   templateUrl: './goal-groups.component.html',
   styleUrl: './goal-groups.component.scss'
 })
@@ -33,5 +34,16 @@ export class GoalGroupsComponent {
 
   getTransparentColor(group: IGoalGroup): string {
     return ColorUtils.hexToRgba(group.project?.color || ColorUtils.defaultColor, ColorUtils.defaultOpacity);
+  }
+
+  getLabelData(goal: IGoalFlat): ILabelData {
+    const labelItems = [];
+    labelItems.push({
+      text: goal.name,
+      color: '#000000',
+      background: '#eeeeee',
+    });
+
+    return { data: labelItems };
   }
 }
