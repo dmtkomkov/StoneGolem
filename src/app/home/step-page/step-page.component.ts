766235fde52f2a048dd814b3aae07a9e1e14108a
Dmitry Komkov <dmtkomkov@gmail.com>
2025-08-13 16:33:03 +0200

    Return 0 minutes

--- src/app/home/step-page/step-page.component.ts
+++ src/app/home/step-page/step-page.component.ts
@@ -29,7 +29,7 @@ interface StepForm {
 })
 export class StepPageComponent {
   hourOptions: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-  minuteOptions: number[] = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+  minuteOptions: number[] = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
   stepForm!: FormGroup<StepForm>;
   users!: IUser[];
   currentUser!: IUser;
